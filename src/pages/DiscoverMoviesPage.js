a26f585a6382fa257811f0ad2ab1cb3f2e848606
VaporyPower72 <signupalex19@gmail.com>
2020-07-03 14:39:15 +0200

    Added search bar to movies page

--- src/pages/DiscoverMoviesPage.js
+++ src/pages/DiscoverMoviesPage.js
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function DiscoverMoviePage() {
-  return <div>Discover</div>;
+export default function DiscoverMoviesPage() {
+  const [searchText, set_searchText] = useState("");
+
+  const search = () => {
+    console.log("TODO search movies for:", searchText);
+  };
+
+  return (
+    <div>
+      <h1>Discover some movies!</h1>
+      <p>
+        <input
+          value={searchText}
+          onChange={(e) => set_searchText(e.target.value)}
+        />
+        <button onClick={search}>Search</button>
+      </p>
+    </div>
+  );
 }
